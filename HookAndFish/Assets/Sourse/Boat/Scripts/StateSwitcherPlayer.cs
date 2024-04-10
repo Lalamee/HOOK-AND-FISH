58767c8e3874c7e12257e90662b1f9aed165b565
Lalamee <kurilov6dv@gmail.com>
2024-04-11 02:50:21 +0300

    fix rotatation and move

--- HookAndFish/Assets/Sourse/Boat/Scripts/StateSwitcherPlayer.cs
+++ HookAndFish/Assets/Sourse/Boat/Scripts/StateSwitcherPlayer.cs
@@ -1,39 +1,32 @@
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 [RequireComponent(typeof(BoatMover))]
 public class StateSwitcherPlayer : MonoBehaviour
 {
-    [SerializeField] private Hook _hook;
-    [SerializeField] private HarpoonControl _harpoonControl;
-    [SerializeField] private Laser _laser;
-    [SerializeField] private float rotationSpeed = 5f; 
-
+    private HarpoonControl _harpoonControl;
     private BoatMover _boatMover;
-
-    private bool isRotating = false; 
-    private Quaternion desiredRotation;
-    private Vector3 desiredPosition;
+    private Laser _laser;
+    private Hook _hook;
+    private Vector3 _targetPosition;
+    private float rotationSpeed = 5f;
+    private float moveSpeed = 5f; 
+    private bool _isProcessing = false;
 
     private void Start()
     {
         _boatMover = GetComponent<BoatMover>();
+
+        _harpoonControl = FindObjectOfType<HarpoonControl>();
+        _hook = FindObjectOfType<Hook>();
+        _laser = FindObjectOfType<Laser>();
     }
 
     private void Update()
     {
-        if (isRotating)
+        if (_isProcessing)
         {
-            // Плавное вращение персонажа вокруг оси Z
-            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
-
-            // Проверка, достиг ли персонаж желаемого поворота
-            if (Quaternion.Angle(transform.rotation, desiredRotation) < 0.1f)
-            {
-                isRotating = false;
-                _hook.enabled = true;
-                _laser.OnRenderer();
-                _harpoonControl.enabled = true;
-            }
+            RotateAndMoveToCenter();
         }
     }
 
@@ -41,17 +34,30 @@ public class StateSwitcherPlayer : MonoBehaviour
     {
         if (collider.gameObject.TryGetComponent(out AreaForBoat areaForBoat))
         {
-            RotateToAxisZ(areaForBoat);
-            _boatMover.enabled = false;
+            StartProcessing(areaForBoat.transform.position);
             areaForBoat.StartSpawnFish();
         }
     }
-    
-    private void RotateToAxisZ(AreaForBoat area)
-    {
-        isRotating = true;
 
-        Vector3 targetDirection = Vector3.forward;
-        desiredRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+    private void StartProcessing(Vector3 centerPosition)
+    {
+        _isProcessing = true;
+        _targetPosition = centerPosition;
+        _boatMover.enabled = false;
+    }
+
+    private void RotateAndMoveToCenter()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+
+        if (Quaternion.Angle(transform.rotation, targetRotation) < 1.1f)
+        {
+            _isProcessing = false;
+            _hook.enabled = true;
+            _laser.OnRenderer();
+            _harpoonControl.enabled = true;
+        }
     }
 }
